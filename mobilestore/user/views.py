d5096b2be3850b4bcf7ad24d2bfb5db6cf5fa36e
akshaya <akshayakrishna54545454@gmail.com>
2023-08-06 12:48:47 -1200

    mbstr

---
+++ mobilestore/user/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,redirect
+from django.views.generic import View,CreateView,TemplateView,FormView,UpdateView
+from store.models import StoreModel
+from .models import Purchase
+from django.contrib import messages
+
+# Create your views here.
+
+class UserView(View):
+    def get(self,request):
+        prod=StoreModel.objects.all()
+        return render(request,'userhome.html',{'data':prod})
